0828c9bd6b29d31c1201ca451d74244b12d2b1f8
bartk123 <45145305+bartk123@users.noreply.github.com>
2024-05-25 17:56:36 +0200

    Delete src/cw4 directory

--- src/cw4/Observer.java
+++
@@ -1,3 +0,0 @@
-public interface Observer {
-    public void update(double temperature, double humidity, double pressure);
-}
--- src/cw4/WeatherStation.java
+++
@@ -1,8 +0,0 @@
-public class WeatherStation {
-    public static void main(String[] args) {
-        WeatherData weatherData = new WeatherData();
-        CurrentConditionsDisplay ccd = new CurrentConditionsDisplay(weatherData);
-//        ccd.register(weatherData);
-        weatherData.setMeasurements(15,36,1004);
-    }
-}
